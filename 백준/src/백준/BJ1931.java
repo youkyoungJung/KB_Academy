7ed5b2a587d5b262ee9129bd9b83bfe736502d9c
student <student@M4037>
2023-03-17 10:57:11 +0900

    0317

--- 백준/src/백준/BJ1931.java
+++ 백준/src/백준/BJ1931.java
@@ -17,13 +17,14 @@ public class BJ1931 {
 		int[][] A = new int[n][2];
 		for(int i=0; i<n; i++) {
 			st = new StringTokenizer(br.readLine());
-			A[i][0] = Integer.parseInt(st.nextToken());
-			A[i][1] = Integer.parseInt(st.nextToken());
+			A[i][0] = Integer.parseInt(st.nextToken());//회의시작시간
+			A[i][1] = Integer.parseInt(st.nextToken());//회의종료시간
 		}
 		 Arrays.sort(A, new Comparator<int[]>() {
 		      @Override
 		      public int compare(int[] S, int[] E) {
-		        if (S[1] == E[1]) { // 종료 시간이 같을 경우
+		        if (S[1] == E[1]) { // 종료 시간이 같을 경우(현재꺼[1] - 다음꺼[1]
+		        					// [1]인 이유는 종료시간만 비교)
 		          return S[0] - E[0];
 		        }
 		        return S[1] - E[1];
@@ -32,7 +33,7 @@ public class BJ1931 {
 		 int count = 0;
 		 int end = -1;
 		 for(int i=0; i<n; i++) {
-			 if(A[i][0] >= end) {
+			 if(A[i][0] >= end) {//이전회의 종료시간 보다 다음회의 시작시간이 커야함.
 				 end = A[i][1];
 				 count++;
 			 }
